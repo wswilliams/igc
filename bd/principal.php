f91057ecb77c905ac3f0387106c0db77b44053b1
wswilliams <wswilliamssilva938@gmail.com>
2015-05-05 22:22:28 +0100

    controle de login

--- bd/principal.php
+++ bd/principal.php
@@ -4,6 +4,9 @@ function principal() {
 
     if ($_COOKIE["nome"]) {
         $nome = $_COOKIE["nome"];
+        
+        if(strcasecmp ($nome,"WILLIAMSSILVA")==0){
+        
         ?>
         <!DOCTYPE html>
         <html lang="en">
@@ -137,13 +140,14 @@ function principal() {
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="js/ie10-viewport-bug-workaround.js"></script>
     </body>
+          
         
-        
-        
-        
-        <?php
+    <?php
+        }else{
+            header("Location: cadastroigc.php");
+        }
     } else {
-        header("Location: Index.php?");
+        header("Location: ../index.php");
     }
 }
 
--- bd/view/cabec.php
+++ bd/view/cabec.php
@@ -13,7 +13,10 @@
                       <!--<a class="navbar-brand" href="home" id="logo"></a>-->
                       <!--<img id="logo" src="img/tp_home_menu_logo.png" width="50%"/>-->
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
+    <?php
+     if(strcasecmp ($_COOKIE["nome"],"WILLIAMSSILVA")==0){
+       ?>  
+         <div class="collapse navbar-collapse" id="myNavbar">
         <ul id="nava-li-a" class="nav navbar-nav navbar-right" >
             <li>
                 <a href="principal.php">
@@ -68,6 +71,68 @@
         <li><a href="#"><span class="glyphicon glyphicon-user"></span> WebMail</a></li>
       </ul>-->
     </div>
+                 
+     <?php
+     }else{
+         ?>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul id="nava-li-a" class="nav navbar-nav navbar-right" >
+            <li>
+                <a href="#">
+                    <img src="ICON/home.png" width="40" height="30" border="0" title = "Pagina Inicial">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="ICON/flat-35681_150.png" width="40" height="30" border="0" title = "Area de Membro">
+                </a>
+            </li> 
+            <li>
+                <a href="#">
+                    <img src="ICON/internet-37197_150.png" width="40" height="30" border="0" title = "CELULA">
+                </a>
+            </li> 
+            <li>
+                <a href="#">
+                    <img src="ICON/computer-24944_150.png" width="40" height="30" border="0" title = "Escola de Lideres">
+                </a>
+            </li> 
+            <li>
+                <a href="#">
+                    <img src="ICON/button-41706_150.png" width="40" height="30" border="0" title = "CONTAS">
+                </a>
+            </li> 
+            <li>
+                <a href="#">
+                    <img src="ICON/baby-25388_150.png" width="40" height="30" border="0" title = "Aniversariantes">
+                </a>
+            </li> 
+            <li>
+                <a href="#">
+                    <img src="ICON/icon-28157_150.png" width="40" height="30" border="0" title = "Carta de Saida">
+                </a>
+            </li> 
+            <li>
+                <a href="#">
+                    <img src="ICON/icone_email.png" width="40" height="30" border="0" title = "Checar Email">
+                </a>
+            </li> 
+            <!--<li><a href="copyright.php">Copyright</a></li>-->
+            <li>
+                <a href="desconectar.php">
+                    <img src="ICON/sign-36980_640.png" width="40" height="30" border="0" title = "SAIR">
+                </a>
+            </li>
+        
+        <!--<li><a href="http://webmail.tapaproducoes.com.br/" target="_blanck"><span class="glyphicon glyphicon-user"></span> WebMail</a></li>--> 
+      </ul>
+<!--      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> WebMail</a></li>
+      </ul>-->
+    </div>
+       <?php
+        }
+     ?> 
   </div>
 </nav>
 <!--            <img src="images/BANER1.jpg" width="100%" height="80%" border="0">
